8ee8ca3344c9fa6a278a964bf5a7c7b9d8a50093
Louis Chatriot <louis.chatriot@gmail.com>
2015-10-24 23:45:59 +0200

    Can draw stones

--- client/js/goban.js
+++ client/js/goban.js
@@ -3,11 +3,14 @@ function Goban (_opts) {
   this.size = opts.size || 19;
   this.container = opts.container || '#the-goban';
   this.$container = $(this.container);
+  this.pixelSize = opts.pixelSize || this.$container.width();
 
   this.$container.addClass('goban-container');
 
-  this.pixelSize = this.$container.width();
+  this.$container.width(this.pixelSize);
   this.$container.height(this.pixelSize);
+
+  this.stoneSize = this.pixelSize / (this.size - 1);
 }
 
 
@@ -26,3 +29,15 @@ Goban.prototype.drawBoard = function () {
     this.$container.append($line);
   }
 };
+
+
+Goban.prototype.drawStone = function (color, x, y) {
+  var $stone = $('<div class="goban-stone-' + color + '"></div>');
+  $stone.css('width', this.stoneSize + 'px');
+  $stone.css('height', this.stoneSize + 'px');
+  $stone.css('left', ((x - 0.5) * this.stoneSize) + 'px');
+  $stone.css('top', ((y - 0.5) * this.stoneSize) + 'px');
+  this.$container.append($stone);
+};
+
+
--- client/js/index.js
+++ client/js/index.js
@@ -1,4 +1,7 @@
-var goban = new Goban({ size: 6 });
+var goban = new Goban({ size: 19, pixelSize: 700 });
 
 goban.drawBoard();
+goban.drawStone('white', 1, 2);
+goban.drawStone('black', 2, 2);
+goban.drawStone('black', 1, 3);
 
