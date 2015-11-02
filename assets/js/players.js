3c368f2f07dfdeb34865c4a86cdbf7fa8186246a
Louis Chatriot <louis.chatriot@gmail.com>
2015-11-02 19:37:02 +0100

    Players list updates in real time on all browsers

--- assets/js/players.js
+++ assets/js/players.js
@@ -2,5 +2,16 @@ console.log(socket);
 
 
 socket.on('connectedUsers.change', function(m) {
-  console.log(m);
+  //TODO: Switch to a template-based approach
+  var lists = "<b>Connected users</b><ul>";
+  m.connectedUsers.forEach(function (user) {
+    lists += "<li>" + user.name + " - " + user.email + "</li>";
+  });
+  lists += "</ul><b>Disconnected users</b><ul>";
+  m.disconnectedUsers.forEach(function (user) {
+    lists += "<li>" + user.name + " - " + user.email + "</li>";
+  });
+  lists += "</ul>";
+
+  $('#lists').html(lists);
 });
