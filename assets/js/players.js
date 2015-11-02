4b691bed39a4be3749c21c625ff46ba66938b300
Louis Chatriot <louis.chatriot@gmail.com>
2015-11-02 19:47:23 +0100

    Template with Mustache

--- assets/js/players.js
+++ assets/js/players.js
@@ -1,17 +1,5 @@
-console.log(socket);
-
+var listTemplate = $('#listTemplate').html();
 
 socket.on('connectedUsers.change', function(m) {
-  //TODO: Switch to a template-based approach
-  var lists = "<b>Connected users</b><ul>";
-  m.connectedUsers.forEach(function (user) {
-    lists += "<li>" + user.name + " - " + user.email + "</li>";
-  });
-  lists += "</ul><b>Disconnected users</b><ul>";
-  m.disconnectedUsers.forEach(function (user) {
-    lists += "<li>" + user.name + " - " + user.email + "</li>";
-  });
-  lists += "</ul>";
-
-  $('#lists').html(lists);
+  $('#lists').html(Mustache.render(listTemplate, m));
 });
