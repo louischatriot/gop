29350ff8525c918efd2a6426a17d3851408b2098
Louis Chatriot <louis.chatriot@gmail.com>
2015-11-03 18:31:30 +0100

    Better real time lists templates

--- assets/js/players.js
+++ assets/js/players.js
@@ -1,5 +1,10 @@
-var listTemplate = $('#listTemplate').html();
+var listTemplate = $('#listTemplate').html()
+  , connectedInitialData = JSON.parse($('#connectedInitialData').html())
+  , disconnectedInitialData = JSON.parse($('#disconnectedInitialData').html())
+  ;
 
 socket.on('connectedUsers.change', function(m) {
   $('#lists').html(Mustache.render(listTemplate, m));
 });
+
+$('#lists').html(Mustache.render(listTemplate, { connectedUsers: connectedInitialData, disconnectedUsers: disconnectedInitialData }));
