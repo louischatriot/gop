b335e8a90caaa31b9e900055dec251a7c9921c48
Louis Chatriot <louis.chatriot@gmail.com>
2015-11-03 18:04:52 +0100

    Better structure for open challenge

--- assets/js/open-challenges.js
+++ assets/js/open-challenges.js
@@ -1,6 +1,8 @@
-var listTemplate = $('#listTemplate').html();
+var listTemplate = $('#listTemplate').html()
+  , initialList = $('#listInitialData').html();
 
 socket.on('openChallenges.change', function(m) {
   $('#list').html(Mustache.render(listTemplate, m));
 });
 
+$('#list').html(Mustache.render(listTemplate, JSON.parse(initialList)));
