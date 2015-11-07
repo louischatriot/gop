e07f5e2fe5e5b3c9a9eac6334b055e6878e06f31
Louis Chatriot <louis.chatriot@gmail.com>
2015-11-07 13:02:00 +0100

    Ugly workaround for broken browsers behavior

--- assets/js/open-challenges.js
+++ assets/js/open-challenges.js
@@ -1,9 +1,17 @@
 var listTemplate = ''
   , initialData = JSON.parse($('#listInitialData').html())
+  , $ghostLink = $('<a href="http://google.fr" target="_blank" id="ghost-link" style="display: none;">Ghost link</a>')
+  , ctrlDown = false;
   ;
 
+$('body').append($ghostLink);
+
 listTemplate += '{{^empty}}<table class="table table-hover"><thead><th>Creator</th><th>Board size</th><th>Name</th></thead><tbody>{{/empty}}'
-listTemplate += '{{^empty}}{{#openChallenges}} <tr href="/web/game/{{_id}}"><td>{{creatorName}}</td>  <td>{{size}}x{{size}}</td>  <td>{{name}}</td></tr> {{/openChallenges}}{{/empty}}'
+listTemplate += '{{^empty}}{{#openChallenges}} <tr href="/web/game/{{_id}}">'
+  listTemplate += '<td>{{creatorName}}</td>'
+  listTemplate += '<td>{{size}}x{{size}}</td>'
+  listTemplate += '<td>{{name}}</td>'
+listTemplate += '{{/openChallenges}}{{/empty}}'
 listTemplate += '{{^empty}}</tbody></table>{{/empty}}'
 listTemplate += '{{#empty}}No open challenge{{/empty}}'
 
@@ -13,12 +21,40 @@ function updateList(data) {
 
   $('tbody tr').css('cursor', 'pointer');
 
+
+  // Super convoluted, but only way to mimic mouse actions behavior
   $('tr').on('mousedown', function (evt) {
     var url = $(evt.currentTarget).attr('href');
-    if (evt.which === 1) { document.location = url; }
-    if (evt.which === 2) { document.location = url; }   // TODO: find way to open in new tab, maybe creating a transient link and simulating middle click
+    if (evt.which === 1) {
+      if (ctrlDown) {
+        openInNewTab(url);
+        ctrlDown = false;
+      } else {
+        openInCurrentTab(url);
+      }
+    }
+    if (evt.which === 2) { openInNewTab(url); }
   });
 }
+
+$(document).on('keydown', function (evt) {
+  if (evt.keyCode === 17) { ctrlDown = true; }
+});
+
+$(document).on('keyup', function (evt) {
+  if (evt.keyCode === 17) { ctrlDown = false; }
+});
+
+function openInCurrentTab (url) {
+  document.location = url;
+}
+
+function openInNewTab (url) {
+  $ghostLink.attr('href', url);
+  $ghostLink[0].click();
+}
+
+
 
 
 socket.on('openChallenges.change', updateList);
